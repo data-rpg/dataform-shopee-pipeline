6d3f6ac8fb5a9e757627a1e1d951f92020aa1d3c
Victor Tan <victor.tan@rpg.ventures>
2023-12-28 01:51:25 +0000

    Declare Shopee raw orders data

---
+++ definitions/sources/shopee-raw-orders.js
@@ -0,0 +1,65 @@
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_blmy",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_blsg",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_ccmy",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_ccsg",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_crmy",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_ebsg",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_mtmy",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_mtsg",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_pamy",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_tmmy",
+});
+
+declare({
+    database: "rpg-dw",
+    schema: "testing",
+    name: "shopee_order_details_tmsg",
+});
